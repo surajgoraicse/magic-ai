e0307ec6d38cda87e393c86991bbe4db405d7d38
surajgoraicse <surajgoraicse@gmail.com>
2025-09-19 08:14:16 +0530

    config shadcn and tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { crx } from "@crxjs/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import manifest from "./manifest.config";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-	plugins: [react(), crx({ manifest })],
+	plugins: [react(), crx({ manifest }), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
